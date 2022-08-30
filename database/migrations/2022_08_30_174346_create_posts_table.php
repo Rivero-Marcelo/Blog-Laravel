a34f1a0be08cb79e5d206376f590447ac957f599
Rivero-Marcelo <104470187+Rivero-Marcelo@users.noreply.github.com>
2022-08-30 15:05:46 -0300

    definicion de modelo Post, User y  controlador de Posts

--- database/migrations/2022_08_30_174346_create_posts_table.php
+++ database/migrations/2022_08_30_174346_create_posts_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
 
-            $table->string('password');
-    
+            $table->string('titulo');
+            $table->text('cuerpo');
+
+            $table->foreignId('user_id')->constrained();
+
+            $table->timestamps();
         });
     }
 
@@ -30,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
