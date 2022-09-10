15cd3c327e277715ff24b824722289767a79801b
Rivero-Marcelo <104470187+Rivero-Marcelo@users.noreply.github.com>
2022-09-09 23:44:58 -0300

    Se agrupron rutas protegidas en web.php

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,18 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('home', [PostController::class, 'index'])->name('home');
-
-Route::get('posts/nuevo', [PostController::class, 'create'])->name('posts.create')->middleware('auth');
-Route::post('posts/nuevo', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
-Route::get('posts/ver/{id}', [PostController::class, 'show'])->name('posts.show')->middleware('auth');
-
-
 Route::post('home', [LoginController::class, 'autenticar'])->name('autenticar');
 
-Route::get('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
+
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('posts/nuevo', [PostController::class, 'create'])->name('posts.create');
+    Route::post('posts/nuevo', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/ver/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+});
 
 Route::get('usuarios/nuevo', [UserController::class, 'create'])->name('usuarios.create');
 Route::post('usuarios/nuevo', [UserController::class, 'store'])->name('usuarios.store');
